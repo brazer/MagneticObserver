66d91c2145e40287026a530849df4e5f1dbce6e2
brazer <salanevich.anatol@gmail.com>
2015-08-14 15:27:11 +0300

    added services of listeners

--- app/src/main/java/by/org/cgm/magneticobserver/services/Topic.java
+++ app/src/main/java/by/org/cgm/magneticobserver/services/Topic.java
@@ -4,12 +4,22 @@ package by.org.cgm.magneticobserver.services;
  * Author: Anatol Salanevich
  * Date: 13.08.2015
  */
-public class Topic {
+public enum Topic {
 
-    public final static String GLOBAL = "global";
+    GLOBAL("global");
+
+    private String name;
+
+    Topic(final String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
 
     public static String[] getTopics() {
-        return new String[] {GLOBAL};
+        return new String[] {Topic.GLOBAL.name};
     }
 
 }
